9cb69fff950287aebc02ec04301f5e4b704a0ab1
Renildo Paes <renildopaesJoob@gmail.com>
2022-05-03 17:39:39 -0300

    2° dia do nlwReturn (incompleto)

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -28,6 +28,15 @@ const feedbackTypes = {
     }
 }
 
+/**
+ * Object.entries(feedbackTypes) => 
+ * [
+ *    ['BUG',   {...}],
+ *    ['IDEA',  {...}],
+ *    ['OTHER', {...}],
+ * ]
+ */
+
 export function WidgetForm(){
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
